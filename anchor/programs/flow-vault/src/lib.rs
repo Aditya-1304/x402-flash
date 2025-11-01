930193060c7fc5113d4246bfb09c3a3ebaf475f9
Aditya Mandal <mandaladitya1614@gmail.com>
2025-11-01 13:19:35 +0530

    init

---
+++ anchor/programs/flow-vault/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("GACf7MzzDobMqJgDGfnaDJe7nCj5UoBoh93xLziomEoX");
+
+#[program]
+pub mod flow_vault {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
